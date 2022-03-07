169389f63d31b9fc497a6dd77d28447d96b59bc1
svarowskey <vladimir.svarowski@gmail.com>
2022-03-07 15:54:55 +0600

    add some styles for client home page

--- src/client/pages/c-page-fitness/c-page-fitness-routing.module.ts
+++ src/client/pages/c-page-fitness/c-page-fitness-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CPageFitnessComponent} from './c-page-fitness.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CPageFitnessComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/client/pages/c-page-food/c-page-food-routing.module.ts
+++ src/client/pages/c-page-food/c-page-food-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CPageFoodComponent} from './c-page-food.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CPageFoodComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
